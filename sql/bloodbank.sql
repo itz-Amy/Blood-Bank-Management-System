6740ff44f841c8667813b0f7362fc3504d149ed3
Amy Ruth Doe Addo <amy.addo@ashesi.edu.gh>
2026-08-12 11:56:18 +0000

    Edit merge issues

--- sql/bloodbank.sql
+++ sql/bloodbank.sql
@@ -586,9 +586,9 @@ VALUES
 ('BT005', 'BT005');
 
 
-
-CREATE FUNCTION fn_DaysBetween(start_date DATE, end_date DATE) RETURNS INT DETERMINISTIC BEGIN RETURN DATEDIFF(end_date, start_date); END; 
-
+DELIMITER //
+CREATE FUNCTION fn_DaysBetween(start_date DATE, end_date DATE) RETURNS INT DETERMINISTIC BEGIN RETURN DATEDIFF(end_date, start_date); END// 
+DELIMITER ;
 
 
 
@@ -603,7 +603,7 @@ BEGIN
 
     SELECT COUNT(*) INTO v_count
     FROM Deferral df
-    LEFT JOIN TemporaryDeferral td ON df.deferralID = td.DeferralId
+    LEFT JOIN TemporaryDeferral td ON df.deferralID = td.deferralID
     WHERE df.donorID = p_donor_id
       AND (td.tempDeferral_id IS NULL OR td.endDate > CURDATE());
 
